f05d3a01779b0d51a0cd245d64947a3ec923e2b6
andyvdv <94682882+andyvdv@users.noreply.github.com>
2023-02-26 17:47:47 +0100

    part2_3

--- part2.py
+++ part2.py
@@ -5,20 +5,20 @@ import matplotlib.pyplot as plt
 from package.gui_utils import load_rirs, plot_rirs
 from package.general import listen_to_array
 from helpers import create_micsigs
+import os
 
-acousticScenario = load_rirs(path="/Users/rudi/Dropbox/MSc EE/P&D ISSP/Brain_Computer_Interface_Directional_Acoustic_Zoom/rirs/rirs_20230217_13h48m45s.pkl.gz")
-
+acousticScenario = load_rirs(path=f"{os.getcwd()}/rirs/rirs_20230225_13h52m06s.pkl.gz")
+print(acousticScenario)
 mic, speech, noise = create_micsigs(acousticScenario=acousticScenario, nmics=2, speechfilenames=["speech1.wav"])
 
-# fig, axs = plt.subplots(3, 1)
-fig = plt.figure()
-ax1 = fig.add_subplot(3, 1, 1)
-ax2 = fig.add_subplot(3, 1, 2,sharex=ax1)
-# ax3 = fig.add_subplot(3, 1, 2,sharex=ax1)
-# ax1.plot(mic[0])
-# ax2.plot(mic[1])
-print(acousticScenario.RIRsAudio.shape)
-ax1.plot(acousticScenario.RIRsAudio[0:1000, 0, 0])
-ax2.plot(acousticScenario.RIRsAudio[0:1000, 1, 0])
+
+plt.plot(acousticScenario.RIRsAudio[0:1000, 0, 0], label = 'Microphone 2')
+plt.plot(acousticScenario.RIRsAudio[0:1000, 1, 0], label='Microphone 1')
+plt.grid()
+plt.title("RIRs of the two microphones with d = 1 cm, fs = 44.1 kHz")
+plt.xlabel('Samples')
+plt.ylabel('RIR')
+plt.legend()
 plt.show()
-listen_to_array(mic[0], 44100)
+
+#listen_to_array(mic[0], 44100)
--- part3.py
+++ part3.py
@@ -4,9 +4,69 @@ import numpy as np
 import matplotlib.pyplot as plt
 from package.gui_utils import load_rirs, plot_rirs
 from package.general import listen_to_array
-from helpers import TDOA_corr, create_micsigs
+from helpers import create_micsigs
+import os
 
-acousticScenario = load_rirs(path="/Users/rudi/Dropbox/MSc EE/P&D ISSP/Brain_Computer_Interface_Directional_Acoustic_Zoom/rirs/rirs_20230217_14h34m33s.pkl.gz")
-corr = TDOA_corr(acousticScenario)
+def TDOA_corr_part3(acousticScenario, audiofile):
+    # First do ground truth sample delay
+    peak1 = np.argmax(acousticScenario.RIRsAudio[:, 0, 0])
+    peak2 = np.argmax(acousticScenario.RIRsAudio[:, 1, 0])
+    TDOA_gt = peak2 - peak1
+    print(f"Ground truth TDOA: {TDOA_gt}")
+    # Crosscorrelation
+    mic, speech, noise = create_micsigs(acousticScenario=acousticScenario,
+                                        nmics=2,
+                                        speechfilenames=[audiofile], 
+                                        noisefilenames=[])
+    corr = ss.correlate(mic[0], mic[1])
+    print(len(mic[0]))
+    print(f"Crosscorrelation maximum value: {np.argmax(corr)}")
 
-print(np.argmax(corr))
+    # Plots
+    fig, (ax1, ax2) = plt.subplots(2, 1)
+    fig.subplots_adjust(hspace=0.5)
+    ax1.plot(corr)
+    ax1.grid(True)
+    ax1.set_ylabel('Cross-correlation amplitude')
+    ax1.set_title('Cross-correlation function')
+    ax2.stem(len(mic[1]) - TDOA_gt - 1, 1)
+    ax2.grid(True)
+    ax2.set_xlabel('Sample index')
+    ax2.set_ylabel('Peak amplitude')
+    ax2.set_title('Expected maximum peak according to the ground truth')
+    plt.show()
+    
+    # Print the estimation error (samples difference)
+    difference = (len(mic[1]) - TDOA_gt) - np.argmax(corr) - 1
+    print(f"DIFFERENCE: {difference} samples")
+    return corr
+
+def exercise3():
+    # Test the file in the non-reverberant scenario
+    audiofile = "whitenoise_signal_1.wav"
+    acousticScenario = load_rirs(path=f"{os.getcwd()}/rirs/part3_135deg.pkl.gz")
+    corr = TDOA_corr_part3(acousticScenario,audiofile)
+    print(np.argmax(corr))
+
+def exercise4():
+    # Increase the reverberation time and check how much reverberation is allowed before the TDOA estimation starts to show errors
+    audiofile = "whitenoise_signal_1.wav"
+    acousticScenario = load_rirs(path=f"{os.getcwd()}/rirs/part3_135deg_0.75rev.pkl.gz")
+    corr = TDOA_corr_part3(acousticScenario,audiofile)
+    print(np.argmax(corr))
+
+def exercise5_6():
+    # Re-simulate the non-reverb scenario but with a speech signal
+    audiofile = "part1_track1_dry.wav"
+    acousticScenario = load_rirs(path=f"{os.getcwd()}/rirs/part3_135deg_0.21rev.pkl.gz") # Can increase the reverberation time here
+    corr = TDOA_corr_part3(acousticScenario,audiofile)
+    print(np.argmax(corr))
+
+
+
+if __name__ == "__main__":
+    #exercise3()
+    #exercise4()
+    exercise5_6()
+
+
